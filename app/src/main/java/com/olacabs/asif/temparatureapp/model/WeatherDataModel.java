f7beb6217786b3ba642c9d29a7bafc80544f4e91
Asif Shaikh <sheikhaasif01@gmail.com>
2018-03-27 20:24:25 +0530

    Initial Commit

---
+++ app/src/main/java/com/olacabs/asif/temparatureapp/model/WeatherDataModel.java
@@ -0,0 +1,9 @@
+package com.olacabs.asif.temparatureapp.model;
+
+/**
+ * Created by asif.shaikh2 on 27/03/18.
+ */
+
+public class WeatherDataModel {
+
+}
---
+++ app/src/main/java/com/olacabs/asif/temparatureapp/viewmodel/ViewModel.java
@@ -0,0 +1,9 @@
+package com.olacabs.asif.temparatureapp.viewmodel;
+
+/**
+ * Created by asif.shaikh2 on 27/03/18.
+ */
+
+public class ViewModel {
+
+}
